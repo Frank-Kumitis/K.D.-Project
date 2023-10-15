9e09bf4e773187e60820c282f52e27a77930ec3a
frankkumitis <frankkumitis@gmail.com>
2023-10-15 12:24:50 -0400

    Dropped df 'Hand' for redundancy.
    Discretized Age into Age Group, using equal-width bins.

--- Phase 2.py
+++ Phase 2.py
@@ -5,12 +5,13 @@ import numpy as np
 # If you do not go through this process, the file will not work as the encoded ASNI nature of the base file cannot be used with pandas
 
 # Read csv and set columns
-data = pd.read_csv('C:\\Users\\Evan Mitchell\\OneDrive\\Documents\\Knowledge Discovery and Data Mining\\Project\\oasis_longitudinal_demographics.csv')
+data = pd.read_csv('/Users/frankkumitis/Desktop/KD & DM Term Project/oasis_longitudinal_demographics_conv.csv')
 data.columns = ['Subject ID', 'MRI ID',	'Group', 'Visit', 'MR Delay', 'M/F', 'Hand', 'Age', 'EDUC',	'SES',	'MMSE',	'CDR',	'eTIV',	'nWBV',	'ASF']
 
-# Drop Subject ID and MRI ID
+# Drop Subject ID, MRI ID, and Hand
 data = data.drop(['Subject ID'], axis=1)
 data = data.drop(['MRI ID'], axis=1)
+data = data.drop(['Hand'], axis=1)
 print('Number of instances = %d' % (data.shape[0]))
 print('Number of attributes = %d' % (data.shape[1]))
 print(data.head())
@@ -34,3 +35,11 @@ print('Number of duplicate rows = %d' % (dups.sum()))
 print('Number of rows before discarding duplicates = %d' % (data.shape[0]))
 data2 = data.drop_duplicates()
 print('Number of rows after discarding duplicates = %d' % (data2.shape[0]))
+
+# Discretize Age into 5 equal-width bins
+print('\nBefore Discretization:')
+print(data2['Age'])
+data2['Age'] = pd.cut(data2['Age'],5,labels=[1.0,2.0,3.0,4.0,5.0])
+data2.rename(columns={'Age':'Age Group'},inplace=True)
+print('\nAfter Discretization:')
+print(data2['Age Group'])
